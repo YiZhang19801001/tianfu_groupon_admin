4ca341252b07110e9e0e9a7e7053df527280b597
china9911 <yizhang19801001@outlook.com>
2019-07-07 15:10:05 +1000

    Poli.php -- remove quantity change in stock

--- app/Classes/Poli.php
+++ app/Classes/Poli.php
@@ -2,7 +2,6 @@
 namespace App\Classes;
 
 use App\Order;
-use App\OrderProduct;
 
 class Poli
 {
@@ -70,16 +69,16 @@ class Poli
         $status = $response->TransactionStatus;
         if ($status === 'Completed') {
             $order = Order::where("payment_code", $response->TransactionRefNo)->first();
-            //todo:: change quantity in stock
-            $orderProducts = OrderProduct::where('order_id', $order->order_id)->get();
-            foreach ($orderProducts as $orderProduct) {
-                $qty = $orderProduct->quantity;
+            // //todo:: change quantity in stock -- quantity in stock will be changed when order created
+            // $orderProducts = OrderProduct::where('order_id', $order->order_id)->get();
+            // foreach ($orderProducts as $orderProduct) {
+            //     $qty = $orderProduct->quantity;
 
-                $product_discount = ProductDistount::find($orderProduct->product_discount_id);
+            //     $product_discount = ProductDistount::find($orderProduct->product_discount_id);
 
-                $product_discount->decrement('quantity', $qty);
-                $product_discount->save();
-            }
+            //     $product_discount->decrement('quantity', $qty);
+            //     $product_discount->save();
+            // }
             if ($order !== null) {
                 $order->order_status_id = 2;
                 $order->save();
